f143a403527f817cc0c163c438f11ede2920c26a
luvbud <young15@chungbuk.ac.kr>
2020-05-07 23:42:26 +0900

    react

--- src/main/java/com/yujachan/trendywords/Controller/TrendyWordsController.java
+++ src/main/java/com/yujachan/trendywords/Controller/TrendyWordsController.java
@@ -24,8 +24,8 @@ public class TrendyWordsController {
 
     @GetMapping("/index")
     public String index() {
-        //return "index";
-        return "안녕하세요 현재 서버시간은 " + new Date() + "입니다.\n";
+        return "index";
+        //return "안녕하세요 현재 서버시간은 " + new Date() + "입니다.\n";
     }
 
 
